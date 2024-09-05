8ef390d8833a358ac5d1619b9d76fe4f3871f14a
Sevastian Cañari <sevastianprime@outlook.com>
2024-09-05 15:10:18 -0500

    second commmit

---
+++ src/functions/testTrigger.ts
@@ -0,0 +1,10 @@
+import { app, InvocationContext, Timer } from "@azure/functions";
+
+export async function testTrigger(myTimer: Timer, context: InvocationContext): Promise<void> {
+    context.log('Timer function processed request!!!.');
+}
+
+app.timer('testTrigger', {
+    schedule: '0 */5 * * * *',
+    handler: testTrigger
+});
